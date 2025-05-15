ad05d3fd4d6162565b4e4c9f28cc4e2843a4824b
Joe Corall <jjc223@lehigh.edu>
2025-05-15 10:12:46 -0400

    go get -u && go mod vendor

--- go.mod
+++ go.mod
@@ -1,15 +1,19 @@
-module github.com/wiltonsr/ldapAuth
+module github.com/lehigh-university-libraries/ldapAuth
 
-go 1.17
+go 1.23.0
+
+toolchain go1.24.2
 
 require (
-	github.com/go-ldap/ldap/v3 v3.4.4
-	github.com/gorilla/securecookie v1.1.1
-	github.com/gorilla/sessions v1.2.1
+	github.com/go-ldap/ldap/v3 v3.4.11
+	github.com/gorilla/securecookie v1.1.2
+	github.com/gorilla/sessions v1.4.0
+	github.com/wiltonsr/ldapAuth v0.1.10
 )
 
 require (
-	github.com/Azure/go-ntlmssp v0.0.0-20220621081337-cb9428e4ac1e // indirect
-	github.com/go-asn1-ber/asn1-ber v1.5.4 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
+	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.8-0.20250403174932-29230038a667 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 )
--- vendor/github.com/go-ldap/ldap/v3/client.go
+++ vendor/github.com/go-ldap/ldap/v3/client.go
@@ -1,6 +1,7 @@
 package ldap
 
 import (
+	"context"
 	"crypto/tls"
 	"time"
 )
@@ -9,7 +10,8 @@ import (
 type Client interface {
 	Start()
 	StartTLS(*tls.Config) error
-	Close()
+	Close() error
+	GetLastError() error
 	IsClosing() bool
 	SetTimeout(time.Duration)
 	TLSConnectionState() (tls.ConnectionState, bool)
@@ -26,10 +28,15 @@ type Client interface {
 	Modify(*ModifyRequest) error
 	ModifyDN(*ModifyDNRequest) error
 	ModifyWithResult(*ModifyRequest) (*ModifyResult, error)
+	Extended(*ExtendedRequest) (*ExtendedResponse, error)
 
 	Compare(dn, attribute, value string) (bool, error)
 	PasswordModify(*PasswordModifyRequest) (*PasswordModifyResult, error)
 
 	Search(*SearchRequest) (*SearchResult, error)
+	SearchAsync(ctx context.Context, searchRequest *SearchRequest, bufferSize int) Response
 	SearchWithPaging(searchRequest *SearchRequest, pagingSize uint32) (*SearchResult, error)
+	DirSync(searchRequest *SearchRequest, flags, maxAttrCount int64, cookie []byte) (*SearchResult, error)
+	DirSyncAsync(ctx context.Context, searchRequest *SearchRequest, bufferSize int, flags, maxAttrCount int64, cookie []byte) Response
+	Syncrepl(ctx context.Context, searchRequest *SearchRequest, bufferSize int, mode ControlSyncRequestMode, cookie []byte, reloadHint bool) Response
 }
